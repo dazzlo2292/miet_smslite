625ae0c5e2cfe394636a4e3d2bbbb75e1cf85200
Николай Шатских <nikolay.shatskikh@mfms.ru>
2022-03-21 13:58:15 +0300

    add validation text and address

--- src/miet/main/SendMessages.java
+++ src/miet/main/SendMessages.java
@@ -7,25 +7,34 @@ public class SendMessages {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите текст сообщения:");
         String newText = scan.nextLine();
-        System.out.print("");
-        System.out.println("Введите номер телефона абонента:");
-        String newAddress = scan.next();
-
-        Message newMessage = new Message(newText, newAddress);
-
-        Thread.sleep(3000L);
-        System.out.println("На номер " + newMessage.getAddress() + " отправлено следующее сообщение:");
-        System.out.println("\"" + newMessage.getText() + "\"");
-
-        int rand = (int) (Math.random() * 10);
-        if (rand < 6) {
-            newMessage.setStatus("Доставлено");
+        if (newText.isEmpty() || newText.length() > 2000) {
+            System.err.println("Введён некорректный текст!\n");
+            SendMessages.sendMessages();
         } else {
-            newMessage.setStatus("Недоставлено");
+            System.out.print("");
+            System.out.println("Введите номер телефона абонента:");
+            String newAddress = scan.next();
+            if (newAddress.length() != 10 || newAddress.charAt(0) == '0') {
+                System.err.println("Введён некорретный номер!\n");
+                SendMessages.sendMessages();
+            } else {
+
+                Message newMessage = new Message(newText, newAddress);
+
+                Thread.sleep(3000L);
+                System.out.println("На номер " + newMessage.getAddress() + " отправлено следующее сообщение:");
+                System.out.println("\"" + newMessage.getText() + "\"");
+
+                int rand = (int) (Math.random() * 10);
+                if (rand < 6) {
+                    newMessage.setStatus("Доставлено");
+                } else {
+                    newMessage.setStatus("Недоставлено");
+                }
+
+                Thread.sleep(3000L);
+                System.out.println("Статус: " + newMessage.getStatus());
+            }
         }
-
-        Thread.sleep(3000L);
-        System.out.println("Статус: " + newMessage.getStatus());
-
     }
 }
